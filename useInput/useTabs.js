bffbd593680bb049ce13847768a2abfcf156272d
김하진 <knj9417@gmail.com>
2022-02-20 21:09:19 +0900

    useTabs

--- useInput/useTabs.js
+++ useInput/useTabs.js
@@ -7,15 +7,3 @@ export const useTabs = (initialTab, allTabs) => {
     currentItem: allTabs[currentIndex],
   };
 };
-
-const App = () => {
-  const { currentItem } = useTabs(0, content);
-  return (
-    <div className="App">
-      {content.map((section) => (
-        <button>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
-    </div>
-  );
-};
