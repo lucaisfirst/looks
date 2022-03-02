3352a8e3135535d8677e045eae75a60ccaba521b
김하진 <knj9417@gmail.com>
2022-03-02 18:29:01 +0900

    useConfirm upgrage

--- useConfirm/useConfirm.js
+++ useConfirm/useConfirm.js
@@ -1,14 +1,17 @@
-const useConfirm = (message = "", callback, rejection) => {
-  if (typeof callback !== "function") {
+export const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function") {
+    return;
+  }
+  if (onCancel && typeof onConfirm !== "function") {
     return;
   }
   const confirmAction = () => {
     if (confirm(message)) {
-      callback();
+      onConfirm();
     } else {
-      rejection();
+      onCancel();
     }
   };
   return confirmAction;
 };
-//useConfirm 이용할 때 message 입력하고, callback함수와 rejection 함수 만들어서 연결하면 된다.
+//useConfirm 이용할 때 message 입력하고, onConfirm에 해당하는 함수와 onCancel에 해당하는 함수 만들어서 연결하면 된다.
