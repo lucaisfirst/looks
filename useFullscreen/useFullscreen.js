f71ef5187d61c139e26a5a71a5d123e968c64f59
김하진 <knj9417@gmail.com>
2022-03-06 17:27:43 +0900

    useFullscreen

---
+++ useFullscreen/useFullscreen.js
@@ -0,0 +1,12 @@
+export const useFullscreen = () => {
+  const element = useRef();
+  const triggerFull = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+  };
+  return { element, triggerFull, exitFull };
+};
