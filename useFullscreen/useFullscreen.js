1fce833849ca0b19d3047558cd2d18107a1b75e4
김하진 <knj9417@gmail.com>
2022-03-15 19:10:59 +0900

    useFullscreen browser compatibility

--- useFullscreen/useFullscreen.js
+++ useFullscreen/useFullscreen.js
@@ -1,18 +1,38 @@
-export const useFullscreen = (callback) => {
+const useFullscreen = (callback) => {
   const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
   const triggerFull = () => {
     if (element.current) {
-      element.current.requestFullscreen();
-      if (callback && typeof callback === "function") {
-        callback(true);
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullScreen) {
+        element.current.webkitRequestFullScreen();
+      } else if (element.current.msRequestFullScreen) {
+        element.current.msRequestFullScreen();
       }
+      runCb(true);
     }
   };
   const exitFull = () => {
     document.exitFullscreen();
-    if (callback && typeof callback === "function") {
-      callback(false);
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullScreen) {
+      document.webkitExitFullScreen();
+    } else if (document.msExitFullScreen) {
+      document.msExitFullScreen();
+    }
+    runCb(false);
   };
   return { element, triggerFull, exitFull };
 };
-//callback 함수 받아서 만든 모델 여기에 모든 브라우저에서 호환되게 조건 걸어야 됨.
+
+//callback 함수 받아서 만든 모델 여기에 모든 브라우저에서 호환되게 조건 걸어야 됨. => complete
